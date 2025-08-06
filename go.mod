31bde621c2b87eff3055612fe08b5c498469a668
Tom Ecrepont <tomecrepont@gmail.com>
2025-08-06 10:01:53 +0000

    Add Go module and Portal model structure

---
+++ go.mod
@@ -0,0 +1,23 @@
+module github.com/troptropcontent/qr_code_maintenance
+
+go 1.24.5
+
+require (
+	github.com/a-h/templ v0.3.924
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/labstack/echo/v4 v4.13.4
+	github.com/lib/pq v1.10.9
+)
+
+require (
+	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/mattn/go-colorable v0.1.14 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	github.com/valyala/bytebufferpool v1.0.0 // indirect
+	github.com/valyala/fasttemplate v1.2.2 // indirect
+	golang.org/x/crypto v0.38.0 // indirect
+	golang.org/x/net v0.40.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+	golang.org/x/text v0.25.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
+)
---
+++ internal/models/portal.go
@@ -0,0 +1,19 @@
+package models
+
+import (
+	"time"
+)
+
+type Portal struct {
+	ID                int       `json:"id" db:"id"`
+	Name              string    `json:"name" db:"name"`
+	AddressStreet     string    `json:"address_street" db:"address_street"`
+	AddressZipcode    string    `json:"address_zipcode" db:"address_zipcode"`
+	AddressCity       string    `json:"address_city" db:"address_city"`
+	ContractorCompany string    `json:"contractor_company" db:"contractor_company"`
+	ContactPhone      string    `json:"contact_phone" db:"contact_phone"`
+	ContactEmail      string    `json:"contact_email" db:"contact_email"`
+	InstallationDate  time.Time `json:"installation_date" db:"installation_date"`
+	CreatedAt         time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt         time.Time `json:"updated_at" db:"updated_at"`
+}
