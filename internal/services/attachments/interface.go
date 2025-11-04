99dcd62940f5b1079425c0770d489af50557165c
Tom Ecrepont <tomecrepont@gmail.com>
2025-11-04 13:57:51 +0000

    uptdate Attacher.Attach interface

--- internal/services/attachments/interface.go
+++ internal/services/attachments/interface.go
@@ -2,9 +2,9 @@ package attachments
 
 import (
 	"context"
-	"os"
+	"io"
 )
 
 type Attacher interface {
-	Attach(ctx context.Context, file *os.File, record_id uint, record_type string, contentType string) error
+	Attach(ctx context.Context, file io.ReadSeeker, fileName string, holder_id uint, holder_type string, kind string) error
 }
