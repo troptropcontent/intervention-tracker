0138763980ec2c94bf7e955de73a0ce514a532cc
Tom Ecrepont <tomecrepont@gmail.com>
2025-11-04 16:40:27 +0000

    add a new SignedUrl field to Attachement

--- internal/models/attachment.go
+++ internal/models/attachment.go
@@ -22,6 +22,7 @@ type Attachment struct {
 	CreatedAt   time.Time      `json:"created_at"`
 	UpdatedAt   time.Time      `json:"updated_at"`
 	DeletedAt   gorm.DeletedAt `json:"-" gorm:"index"`
+	SignedUrl   string         `gorm:"-"`
 }
 
 func (Attachment) TableName() string {
