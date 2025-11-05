38fb5c8763ae389ff8df28c1a18087ced97c86fb
Tom Ecrepont <tomecrepont@gmail.com>
2025-11-05 14:25:54 +0000

    add new type column

--- internal/models/intervention.go
+++ internal/models/intervention.go
@@ -13,6 +13,13 @@ const (
 	ControlKindOther    ControlKind = "other"
 )
 
+type InterventionType string
+
+const (
+	InterventionTypeMaintenance InterventionType = "maintenance"
+	InterventionTypeRepair      InterventionType = "repair"
+)
+
 type ControlTypesStruct struct {
 	Security []string
 	Other    []string
@@ -69,16 +76,17 @@ var ControlTypesByKind = map[ControlKind][]string{
 type ControlResult *bool
 
 type Intervention struct {
-	ID        uint           `json:"id" gorm:"primaryKey"`
-	Date      time.Time      `json:"date" gorm:"not null"`
-	Summary   *string        `json:"summary"`
-	UserID    uint           `json:"user_id" gorm:"not null"`
-	UserName  string         `json:"user_name" gorm:"not null"`
-	PortalID  uint           `json:"portal_id" gorm:"not null"`
-	Signature string         `json:"signature" gorm:"type:text"`
-	CreatedAt time.Time      `json:"created_at"`
-	UpdatedAt time.Time      `json:"updated_at"`
-	DeletedAt gorm.DeletedAt `json:"-" gorm:"index"`
+	ID        uint             `json:"id" gorm:"primaryKey"`
+	Date      time.Time        `json:"date" gorm:"not null"`
+	Type      InterventionType `json:"type" gorm:"not null"`
+	Summary   *string          `json:"summary"`
+	UserID    uint             `json:"user_id" gorm:"not null"`
+	UserName  string           `json:"user_name" gorm:"not null"`
+	PortalID  uint             `json:"portal_id" gorm:"not null"`
+	Signature string           `json:"signature" gorm:"type:text"`
+	CreatedAt time.Time        `json:"created_at"`
+	UpdatedAt time.Time        `json:"updated_at"`
+	DeletedAt gorm.DeletedAt   `json:"-" gorm:"index"`
 
 	// Relationships
 	Portal      Portal       `json:"portal,omitempty" gorm:"foreignKey:PortalID"`
