59f95bba3525cabdc751161e159bc4c1fe8b43a5
Tom Ecrepont <tomecrepont@gmail.com>
2025-11-07 10:19:14 +0000

    crreate an attachement factory

---
+++ internal/services/tests/factories/attachment.go
@@ -0,0 +1,68 @@
+package factories
+
+import (
+	"github.com/troptropcontent/qr_code_maintenance/internal/models"
+	"gorm.io/gorm"
+)
+
+type AttachmentBuilder struct {
+	attachment *models.Attachment
+}
+
+func NewAttachment() *AttachmentBuilder {
+	return &AttachmentBuilder{
+		attachment: &models.Attachment{
+			HolderType:  "interventions",
+			HolderID:    1,
+			Kind:        "photo",
+			StorageKey:  "test/default.jpg",
+			FileName:    "default.jpg",
+			ContentType: "image/jpeg",
+			FileSize:    1024,
+		},
+	}
+}
+
+func (b *AttachmentBuilder) WithHolderType(holderType string) *AttachmentBuilder {
+	b.attachment.HolderType = holderType
+	return b
+}
+
+func (b *AttachmentBuilder) WithHolderID(holderID uint) *AttachmentBuilder {
+	b.attachment.HolderID = holderID
+	return b
+}
+
+func (b *AttachmentBuilder) WithKind(kind string) *AttachmentBuilder {
+	b.attachment.Kind = kind
+	return b
+}
+
+func (b *AttachmentBuilder) WithStorageKey(storageKey string) *AttachmentBuilder {
+	b.attachment.StorageKey = storageKey
+	return b
+}
+
+func (b *AttachmentBuilder) WithFileName(fileName string) *AttachmentBuilder {
+	b.attachment.FileName = fileName
+	return b
+}
+
+func (b *AttachmentBuilder) WithContentType(contentType string) *AttachmentBuilder {
+	b.attachment.ContentType = contentType
+	return b
+}
+
+func (b *AttachmentBuilder) WithFileSize(fileSize int64) *AttachmentBuilder {
+	b.attachment.FileSize = fileSize
+	return b
+}
+
+func (b *AttachmentBuilder) Build() *models.Attachment {
+	return b.attachment
+}
+
+func (b *AttachmentBuilder) Create(db *gorm.DB) *models.Attachment {
+	db.Create(b.attachment)
+	return b.attachment
+}
